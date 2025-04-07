398a0c8fda5b55fca45f6b4e0280913f50323df0
Alejandra <malejamonroyz@gmail.com>
2025-04-07 13:13:00 +0200

    examen

---
+++ javascript/pruebas/prueba.js
@@ -0,0 +1,17 @@
+const read = require('./leer');
+
+//let valor = Number(require('ingrese numero '))
+
+
+let num;
+let val_abs;
+
+num = Number(read("Ingrese numero: "));
+if (num < 0) {
+    val_abs = num * -1;
+} else {
+    val_abs = num;
+}
+
+console.log("|" + num + "| = " + val_abs);
+console.log(`|${num}| = ${val_abs}`);
